0f02a18d764603062a0f67f7a1360ea66d89ee5e
Dayset <dayset@gmail.com>
2022-05-25 21:36:19 +0300

    Add files via upload
    Reverted base64 to old-style URL links.
    I also removed chat ADMIN messages.
    PM pop sound is loaded by URL.

---
+++ Code.js
@@ -0,0 +1,153 @@
+javascript: function make_message(message) {
+  var html;
+  if (message.type == "MESSAGE" || message.type == "TIPS" || is_album_message_type(message.type)) {
+
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<1>",'g'),"<img border=0 img width=25px src='https://stickers.wiki/static/stickers/icqkolobki/file_262719.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<2>",'g'),"<img border=0 src=\/img\/smiles\/smile147.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<3>",'g'),"<img border=0 src=\/img\/smiles\/smile103.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<4>",'g'),"<img border=0 src=\/img\/smiles\/smile110.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<5>",'g'),"<img border=0 src=\/img\/smiles\/smile114.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<6>",'g'),"<img border=0 src=\/img\/smiles\/smile148.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<7>",'g'),"<img border=0 src=\/img\/smiles\/smile111.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<8>",'g'),"<img border=0 src=\/img\/smiles\/smile127.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<9>",'g'),"<img border=0 src=\/img\/smiles\/smile129.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<10>",'g'),"<img border=0 img width=40px src='https://stickers.wiki/static/stickers/icqkolobki/file_262753.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<11>",'g'),"<img border=0 src=\/img\/smiles\/smile140.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<12>",'g'),"<img border=0 img width=25px src='https://stickers.wiki/static/stickers/icqkolobki/file_262718.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<13>",'g'),"<img border=0 img width=30px src='https://stickers.wiki/static/stickers/icqkolobki/file_262717.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<14>",'g'),"<img border=0 src=\/img\/smiles\/smile106.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<15>",'g'),"<img border=0 src=\/img\/smiles\/smile142.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<16>",'g'),"<img border=0 img width=25px src='https://stickers.wiki/static/stickers/icqkolobki/file_262721.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<17>",'g'),"<img border=0 img width=25px src='https://stickers.wiki/static/stickers/icqkolobki/file_262734.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<18>",'g'),"<img border=0 src=\/img\/smiles\/smile119.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<19>",'g'),"<img border=0 src=\/img\/smiles\/smile141.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<20>",'g'),"<img border=0 img width=30px src='https://stickers.wiki/static/stickers/icqkolobki/file_262716.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<21>",'g'),"<img border=0 src=\/img\/smiles\/smile113.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<22>",'g'),"<img border=0 src=\/img\/smiles\/smile132.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<23>",'g'),"<img border=0 src=\/img\/smiles\/smile150.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<24>",'g'),"<img border=0 src=\/img\/smiles\/smile109.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<25>",'g'),"<img border=0 src=\/img\/smiles\/smile149.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<26>",'g'),"<img border=0 src=\/img\/smiles\/smile141.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<27>",'g'),"<img border=0 src=\/img\/smiles\/smile104.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<28>",'g'),"<img border=0 src=\/img\/smiles\/smile102.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<29>",'g'),"<img border=0 src=\/img\/smiles\/smile136.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<30>",'g'),"<img border=0 src=\/img\/smiles\/smile155.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<31>",'g'),"<img border=0 img width=50px src='https://stickers.wiki/static/stickers/icqkolobki/file_262755.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<32>",'g'),"<img border=0 img width=50px src='https://stickers.wiki/static/stickers/icqkolobki/file_262737.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<33>",'g'),"<img border=0 src=\/img\/smiles\/smile146.gif>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<34>",'g'),"<img border=0 img width=40px src='https://stickers.wiki/static/stickers/icqkolobki/file_262738.gif'>"); 
+    }
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<35>",'g'),"<img border=0 img width=40px src='https://stickers.wiki/static/stickers/icqkolobki/file_262749.gif'>"); 
+    }
+ 
+      if (!is_album_message_type(message.type)) {
+      message.text = message.text.replace(new RegExp("<(\\d+)>",'g'),"<img border=0 src=\/img\/smiles\/smile$1.gif>"); 
+    }
+
+        var nick_style = "color:#" + message.color + "; font-weight:bold;";
+        var message_style = "color:#" + message.color + ";";
+        var message_time = message.time.substr(11, 8);
+
+    if (message.is_private == 0) {
+
+        html = "<span class='" + (message.user_sex == 'f' ? "time_girl" : "time_boy") + "'>(" + message_time  + ")</span>";
+    if (message.user_id > 0) {
+        html += make_dropdown_menu_to_user(message, false);
+        html += "<a style='" + nick_style + ";text-decoration:underline;' href='javascript:writeName(\"" + message.user_nick.replace(/&quot;/g,'\\\"') + "\")'>" + message.user_nick + "</a>";
+      }
+        html += "<span style='" + message_style + "'>: " + make_message_text(message) + "</span>";
+
+    } else {
+
+        var message_month = message.time.substr(5, 2);
+        var message_day   = message.time.substr(8, 2);
+        html = "<span class='" + (message.user_sex == 'f' ? "time_girl" : "time_boy") + "'>(" + message_day + '.' + message_month  + ', ' + message_time + ")</span>";
+    if (message.user_id > 0) {
+        html += make_dropdown_menu_to_user(message, true);
+        var user_nick_escape = escapeQuot(escape_user_nick(message.user_nick));
+        html += "<a style='" + nick_style + ";text-decoration:underline;' href='javascript:header_send_private(" + message.user_id + ", \"" + user_nick_escape + "\")'>" + message.user_nick + "</a>";
+        
+        function beep() {
+        var snd = new  Audio("https://github.com/Dayset/Improved-chat-message-generation/blob/main/pm_sound.mp3?raw=true");  
+        snd.play();
+        }
+        beep();
+    }
+        html += "<span style='" + nick_style + "'>&rarr;" + message.to_user_nick + "</span>";
+        html += "<span style='" + message_style + "'>: " + make_message_text(message) + "</span>";
+    }
+  }
+  if (message.type == "READY") {
+    html = message.text;
+  }
+
+  return html;
+}
